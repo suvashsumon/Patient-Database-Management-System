9f220e97ace40b0e289b1ac65c311b68df9829b7
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2021-05-04 20:56:42 +0600

    dashborad.py modified

--- dashboard.py
+++ dashboard.py
@@ -9,11 +9,22 @@ class UI(QMainWindow):
         super().__init__()
         uic.loadUi("ui/dashboard.ui", self)
 
+        # add button
+        addbutton = self.findChild(QPushButton, 'addrecord')
+        addbutton.clicked.connect(self.add_record)
+
+        # add action
+        addaction = self.findChild(QAction)
+        addaction.triggered.connect(self.add_record)
+
     def location_on_the_screen(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def add_record(self):
+        print("add record clicked")
 
 
 app = QApplication([])
