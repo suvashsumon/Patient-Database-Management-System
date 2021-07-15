1285b99e2fb159e947d79790fca30a44359b2e33
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2021-07-15 11:01:35 +0600

    imageviewer : image not showing problem solved

--- imageViewer.py
+++ imageViewer.py
@@ -6,41 +6,52 @@ from PyQt5.QtGui import QIcon, QPixmap
 import sys
 
 
-class Login(QWidget):
+class ImageViewer(QWidget):
     def __init__(self, images):
         super().__init__()
         uic.loadUi("ui/imageViewer.ui", self)
 
-        self.image = self.findChild(QLabel, 'image')
+        self.images = images
+
+        self.imagefield = self.findChild(QLabel, 'image')
         self.previousBtn = self.findChild(QPushButton, 'previousImage')
         self.nextBtn = self.findChild(QPushButton, 'nextImage')
         self.imageCounter = self.findChild(QLabel, 'imageCounter')
 
-        self.previousBtn.clicked.connect(self.previousImage)
-        self.nextBtn.clicked.connect(self.nextImage)
+        self.previousBtn.clicked.connect(self.loadPreviousImage)
+        self.nextBtn.clicked.connect(self.loadNextImage)
 
         self.counter = 0
-        self.total = len(images)
-        self.pixmap = QPixmap(self.images[self.counter])
-        self.image.setPixmap(self.pixmap)
-        self.image.setScaledContents(True)
+        self.total = len(self.images)
+        self.pixmap = QPixmap("/home/suvashkumar/Desktop/programming/pdm_database/images/3505_Mri_0.jpg")
+        self.imagefield.setPixmap(self.pixmap)
+        self.imagefield.setScaledContents(True)
+        self.imageCounter.setText(str(self.counter+1) + "/" + str(self.total))
 
 
-    def previousImage(self):
+    def loadPreviousImage(self):
         self.counter -= 1
         if self.counter < 0:
             self.counter = self.total - 1
         self.pixmap = QPixmap(self.images[self.counter])
-        self.image.setPixmap(self.pixmap)
+        self.imagefield.setPixmap(self.pixmap)
+        self.imageCounter.setText(str(self.counter+1) + "/" + str(self.total))
 
-    def nextImage(self):
+    def loadNextImage(self):
         self.counter += 1
         if self.counter > self.total - 1:
             self.counter = 0
         self.pixmap = QPixmap(self.images[self.counter])
-        self.image.setPixmap(self.pixmap)
+        self.imagefield.setPixmap(self.pixmap)
+        self.imageCounter.setText(str(self.counter+1) + "/" + str(self.total))
 
 app = QApplication([])
-window = Login()
+images = [
+    "/home/suvashkumar/Desktop/programming/pdm_database/images/3505_Mri_0.jpg",
+    "/home/suvashkumar/Desktop/programming/pdm_database/images/3508_Pics_0.jpg",
+    "/home/suvashkumar/Desktop/programming/pdm_database/images/hello.png",
+    "/home/suvashkumar/Desktop/programming/pdm_database/images/3508_Mri_0.png"
+]
+window = ImageViewer(images)
 window.show()
 app.exec_()
