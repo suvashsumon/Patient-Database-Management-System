fadda75ef669dadc25f970adeadacc57ceb8e6bf
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2021-07-20 21:04:48 +0600

    data printing system implimented

--- makepdf.py
+++ makepdf.py
@@ -1,4 +1,7 @@
 import sqlite3
+
+from fpdf import FPDF
+
 from userdataclass import jsondata
 
 
@@ -36,10 +39,131 @@ class MakePdf:
         conn.close()
 
     def printwork(self):
-        print(self.rediology)
+        pdf = FPDF(orientation='P', unit='mm', format='A4')
+        pdf.set_auto_page_break(auto=True, margin=10)
+        pdf.add_page()
+        pdf.set_image_filter("DCTDecode")
+
+        # heading -> Personal Information
+        pdf.set_font("times", 'B', size=12)
+        pdf.set_draw_color(0, 0, 0)
+        pdf.set_fill_color(0, 0, 0)
+        pdf.set_text_color(255, 255, 255)
+        pdf.set_line_width(1)
+        pdf.cell(190, 5, txt="Personal Information", border=1, ln=1, align="L", fill=1)
+
+        # content -> personal Information
+        pdf.set_text_color(0, 0, 0)
+        pdf.set_font("Helvetica", 'B', size=11)
+        pdf.cell(30, 6, txt="ID:", ln=0, align="L")
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.cell(50, 6, txt=str(self.id), ln=0, align='L')
+        pdf.set_font("Helvetica", 'B', size=11)
+        pdf.cell(30, 6, txt="Name:", ln=0, align="L")
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.cell(50, 6, txt=self.name, ln=1, align='L')
+        pdf.set_font("Helvetica", 'B', size=11)
+        pdf.cell(30, 5, txt="Gender:", ln=0, align="L")
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.cell(50, 5, txt=self.sex, ln=0, align='L')
+        pdf.set_font("Helvetica", 'B', size=11)
+        pdf.cell(30, 5, txt="Age:", ln=0, align="L")
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.cell(50, 5, txt=self.age, ln=1, align='L')
+        pdf.set_font("Helvetica", 'B', size=11)
+        pdf.cell(30, 5, txt="Address:", ln=0, align="L")
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.cell(50, 5, txt=self.address, ln=1, align='L')
+        pdf.cell(190, 2, "", ln=1)
+
+        # Heading -> C/C
+        pdf.set_font("Helvetica", 'B', size=12)
+        pdf.set_draw_color(0, 0, 0)
+        pdf.set_fill_color(0, 0, 0)
+        pdf.set_text_color(255, 255, 255)
+        pdf.set_line_width(1)
+        pdf.cell(190, 5, txt="C/C", border=1, ln=1, align="L", fill=1)
+
+        # content -> c/c
+        pdf.set_text_color(0, 0, 0)
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.multi_cell(190, 5,txt=self.cc,ln=0, align="L")
+        pdf.cell(190, 2, "", ln=1)
+
+        # Heading -> OE
+        pdf.set_font("Helvetica", 'B', size=12)
+        pdf.set_draw_color(0, 0, 0)
+        pdf.set_fill_color(0, 0, 0)
+        pdf.set_text_color(255, 255, 255)
+        pdf.set_line_width(1)
+        pdf.cell(190, 5, txt="O.E.", border=1, ln=1, align="L", fill=1)
+
+        # content -> OE
+        pdf.set_text_color(0, 0, 0)
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.multi_cell(190, 5, txt=self.oe, ln=0, align="L")
+        pdf.cell(190, 2, "", ln=1)
+
+        # Heading -> rf
+        pdf.set_font("Helvetica", 'B', size=12)
+        pdf.set_draw_color(0, 0, 0)
+        pdf.set_fill_color(0, 0, 0)
+        pdf.set_text_color(255, 255, 255)
+        pdf.set_line_width(1)
+        pdf.cell(190, 5, txt="R.F.", border=1, ln=1, align="L", fill=1)
+
+        # content -> rf
+        pdf.set_text_color(0, 0, 0)
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.multi_cell(190, 5,txt=self.rf, ln=0, align="L")
+        pdf.cell(190, 2, "", ln=1)
+
+        # Heading -> path report
+        pdf.set_font("Helvetica", 'B', size=12)
+        pdf.set_draw_color(0, 0, 0)
+        pdf.set_fill_color(0, 0, 0)
+        pdf.set_text_color(255, 255, 255)
+        pdf.set_line_width(1)
+        pdf.cell(190, 5, txt="Pathology Reoprt", border=1, ln=1, align="L", fill=1)
+
+        # content -> path report
+        pdf.set_text_color(0, 0, 0)
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.multi_cell(190, 5, txt=self.pathreport, ln=0, align="L")
+        pdf.cell(190, 2, "", ln=1)
+
+        # Heading -> Dxs
+        pdf.set_font("Helvetica", 'B', size=12)
+        pdf.set_draw_color(0, 0, 0)
+        pdf.set_fill_color(0, 0, 0)
+        pdf.set_text_color(255, 255, 255)
+        pdf.set_line_width(1)
+        pdf.cell(190, 5, txt="Dxs", border=1, ln=1, align="L", fill=1)
+
+        # content -> Dxs
+        pdf.set_text_color(0, 0, 0)
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.multi_cell(190, 5, txt=self.dxs, ln=0, align="L")
+        pdf.cell(190, 2, "", ln=1)
+
+        # Heading -> comment
+        pdf.set_font("Helvetica", 'B', size=12)
+        pdf.set_draw_color(0, 0, 0)
+        pdf.set_fill_color(0, 0, 0)
+        pdf.set_text_color(255, 255, 255)
+        pdf.set_line_width(1)
+        pdf.cell(190, 5, txt="Comment", border=1, ln=1, align="L", fill=1)
+
+        # content -> comment
+        pdf.set_text_color(0, 0, 0)
+        pdf.set_font("Helvetica", '', size=11)
+        pdf.multi_cell(190, 5, txt=self.comments, ln=0, align="L")
+        pdf.cell(190, 2, "", ln=1)
+
+        pdf.output("/home/suvashkumar/Desktop/simple_demo.pdf")
 
 
 
 if __name__ == "__main__":
-    obj = MakePdf(2013)
+    obj = MakePdf(2113)
     obj.printwork()
